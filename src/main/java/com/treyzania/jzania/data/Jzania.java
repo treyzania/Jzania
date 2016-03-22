5025fec7509148ba866221a8bf58a3b031d44c81
treyzania <treyzania@gmail.com>
2016-03-21 21:48:57 -0400

    Added main API class.

---
+++ src/main/java/com/treyzania/jzania/data/Jzania.java
@@ -0,0 +1,28 @@
+package com.treyzania.jzania.data;
+
+import java.util.logging.Logger;
+
+public class Jzania {
+
+	private static boolean inited = false;
+	
+	private static Logger log;
+	
+	public static void init() {
+		
+		if (inited) return;
+		
+		log = Logger.getLogger("Jzania");
+		
+		// Add more things as necessary.
+		
+		inited = true;
+		log.info("Jzania initalized.");
+		
+	}
+	
+	public static Logger getLogger() {
+		return log;
+	}
+	
+}
